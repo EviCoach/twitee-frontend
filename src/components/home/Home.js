d4e276167030f4fd07b8c03405d492fdafea84bd
Evi Owhotemu <evicoach@gmail.com>
2022-04-11 13:02:18 +0100

    Updated

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,17 +1,27 @@
 import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import "./Home.css";
 // import close from "../../close.svg";
 // import { transformLayersToAreas } from "../../Utilities/LocationHelper"
 
 
 export default function Home(props) {
-  
+
+    const navigate = useNavigate();
+    const login = (event) => {
+      navigate("/login")
+    }
+    
+    const signup = (event) => {
+        navigate("/sigup")
+    }
 
     return (
         <div className="close-form-container">
-            {/* <img src={close} alt="logo" className="close-form"
-                width={20} height={20} onClick={props.handler} /> */}
-           THIS IS THE HOME
+           
+            <h1>Welcome to Twitee</h1>
+            <div><button onClick={login}>Login</button></div>
+            <div><button onClick={signup}>Signup</button></div>
         </div>
     );
 }
